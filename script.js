b25308ecd9cabddb135e5f5ab4e804cdd8858772
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-06-25 07:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+    function displayBrowserInfo() {
+      const browserName = navigator.appName;
+      const version = navigator.appVersion;
+      const browserInfo = `You are using ${browserName} version ${version}`;
+      
+      const div = document.createElement('div');
+      div.id = 'browser-info';
+      div.textContent = browserInfo;
+      
+      document.body.appendChild(div);
+    }
+
+    displayBrowserInfo();
+  
